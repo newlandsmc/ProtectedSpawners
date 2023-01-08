d6768fd61a09409887931f1df6ce48f6f8639d00
Len <40720638+destro174@users.noreply.github.com>
2023-01-08 10:54:15 +0100

    Prevent protected blocks from exploding

--- src/main/java/com/semivanilla/protectedspawners/listeners/BlockBreakListener.java
+++ src/main/java/com/semivanilla/protectedspawners/listeners/BlockBreakListener.java
@@ -6,17 +6,23 @@ import com.semivanilla.protectedspawners.util.Util;
 import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
 import it.unimi.dsi.fastutil.objects.Object2BooleanOpenHashMap;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.loot.LootTable;
 import org.bukkit.loot.Lootable;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -61,6 +67,16 @@ public class BlockBreakListener implements Listener {
         event.setCancelled(true);
         if (!Config.blockBreakMessage.isEmpty())
             player.sendMessage(Util.parseMiniMessage(Config.blockBreakMessage, null));
+    }
+
+    @EventHandler
+    public void onEntityExplosion(EntityExplodeEvent event) {
+        event.blockList().removeIf(this::unbreakable);
+    }
+
+    @EventHandler
+    public void onEntityExplosion(BlockExplodeEvent event) {
+        event.blockList().removeIf(this::unbreakable);
     }
 
     private boolean unbreakable(Block block) {
